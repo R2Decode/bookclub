cba6da743032995fc0c04eb5f91fd63a4b811895
Sean <seturner@my365.bellevue.edu>
2024-04-13 23:01:12 -0400

    Added Assignment 4.2 updates

--- src/main/java/com/bookclub/BookclubApplication.java
+++ src/main/java/com/bookclub/BookclubApplication.java
@@ -2,12 +2,14 @@ package com.bookclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bookclub.service.impl", "com.bookclub.web"})
 public class BookclubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookclubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookclubApplication.class, args);
+    }
 
 }
